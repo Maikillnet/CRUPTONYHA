d15f84caaee78876181525eea93bd1433e931a91
CKYPSHIK <105435023+Maikillnet@users.noreply.github.com>
2025-09-02 14:12:41 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-# 🚨 ВНИМАНИЕ: здесь лежат секреты. Ты сам просил положить в код.
-
-BOT_TOKEN = "7986877472:AAFd1t_rEhSFAXjueb-Jj-erE_MFZVSeUVA"
-TZ_NAME = "Europe/Moscow"
-POLL_MIN_SEC = 15
-POLL_MAX_SEC = 30
